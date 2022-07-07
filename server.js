aee34ad944a5caba8bad696daa3492d356b75c5d
Donovan <donniemassenburg75@gmail.com>
2022-07-06 20:24:44 -0400

    added cennections to mysql

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+const mysql = require("mysql2");
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: "Massenburg69!",
+    database: "election",
+  },
+  console.log("Connected to the election database.")
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
+
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
+});
